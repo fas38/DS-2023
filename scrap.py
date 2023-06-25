e2488b0066f6cb8ae0dedd002d814d95092be7f7
Sayed Muddashir Hossain <sayed.muddashir@gmail.com>
2023-06-25 13:47:24 +0000

    corrected a small bug

--- scrap.py
+++ scrap.py
@@ -1,39 +1,44 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
 import json
+from scrapy.crawler import CrawlerProcess
 
 
 class StockSpider(scrapy.Spider):
-    name = 'stock_spider'
+    name = "stock_spider"
     start_urls = [
-        'https://stockanalysis.com/stocks/industry/internet-content-and-information/',
-        'https://stockanalysis.com/stocks/industry/entertainment/',
-        'https://stockanalysis.com/stocks/industry/travel-services/'
+        "https://stockanalysis.com/stocks/industry/internet-content-and-information/",
+        "https://stockanalysis.com/stocks/industry/entertainment/",
+        "https://stockanalysis.com/stocks/industry/travel-services/",
     ]
 
-    custom_settings = {
-        'FEED_FORMAT': 'json',
-        'FEED_URI': 'stocks.json'
-    }
+    custom_settings = {"FEED_FORMAT": "csv", "FEED_URI": "stocks.csv"}
 
     def parse(self, response):
-        rows = response.css('table tbody tr')
+        rows = response.css("table tbody tr")
         for row in rows:
-            yield {
-                'symbol': row.css('td:nth-child(2) a::text').get(),
-                'company_name': row.css('td:nth-child(3)::text').get(),
-                'market_cap': row.css('td:nth-child(4)::text').get(),
-                'percent_change': row.css('td:nth-child(5)::text').get(),
-                'volume': row.css('td:nth-child(6)::text').get(),
-                'revenue': row.css('td:nth-child(7)::text').get(),
-            }
+            try:
+                data = {
+                    "symbol": row.css("td:nth-child(2) a::text").get(),
+                    "company_name": row.css("td:nth-child(3)::text").get(),
+                    "market_cap": row.css("td:nth-child(4)::text").get(),
+                    "percent_change": row.css("td:nth-child(5) div::text").get(),
+                    "volume": row.css("td:nth-child(6)::text").get(),
+                    "revenue": row.css("td:nth-child(7)::text").get(),
+                }
+                print(data)
+                yield data
+            except Exception as e:
+                self.log(f"Error while writing to output file: {e}")
+
 
 if __name__ == "__main__":
-    process = CrawlerProcess(settings={
-        "FEEDS": {
-            "stocks.json": {"format": "json"},
-        },
-    })
+    process = CrawlerProcess(
+        settings={
+            "FEEDS": {
+                "stocks.json": {"format": "json"},
+            },
+        }
+    )
 
     process.crawl(StockSpider)
     process.start()  # the script will block here until the crawling is finished
