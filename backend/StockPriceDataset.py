4d568fd727762dd848f7763498c7368f29041562
Fahim Ahmed Shakil <fahimahmed38@gmail.com>
2023-07-06 11:52:47 +0200

    server fully functioning

--- backend/StockPriceDataset.py
+++ backend/StockPriceDataset.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import Dataset
+import pandas as pd
 
 # set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -10,14 +11,54 @@ class StockPriceDataset(Dataset):
         self.data = data
         self.output_dim = output_dim
         self.normalize()
+        self.sequenced_data = self.sequence_data()
 
     def normalize(self):
-        self.data["daily_return"] = (self.data["daily_return"] - self.data["daily_return"].mean()) / self.data["daily_return"].std()
+        
+        # normalize data
+        self.data["daily_return"] = (self.data["daily_return"] - self.data["daily_return"].min()) / (self.data["daily_return"].max() - self.data["daily_return"].min() + 1e-8)
+        self.data["sentiment"] = (self.data["sentiment"] - self.data["sentiment"].min()) / (self.data["sentiment"].max() - self.data["sentiment"].min() + 1e-8)
+        self.data["sentiment_company"] = (self.data["sentiment_company"] - self.data["sentiment_company"].min()) / (self.data["sentiment_company"].max() - self.data["sentiment_company"].min() + 1e-8)
+        self.data["sentiment_industry"] = (self.data["sentiment_industry"] - self.data["sentiment_industry"].min()) / (self.data["sentiment_industry"].max() - self.data["sentiment_industry"].min() + 1e-8)
+
+    # create sequences by seperating each month data into seperate sequence
+    def sequence_data(self):
+        data = self.data
+        list_of_df = []
+        temp = data.copy()
+        temp = temp.reset_index()
+        temp["Date"] = pd.to_datetime(temp["Date"])
+        all_available_dates = temp["Date"].unique()
+    
+        for i in range(len(all_available_dates)):
+            date = all_available_dates[i]
+            temp_df = data[data.index == date]
+            list_of_df.append(temp_df)
+        return list_of_df
+        
 
     def __len__(self):
-        return len(self.data) - self.window_size + 1 - self.output_dim
+        return len(self.sequenced_data) - self.window_size - self.output_dim + 1
 
     def __getitem__(self, idx):
-        x = self.data[idx: idx + self.window_size -1].values # sequence of window_size - 1 days
-        y = self.data.iloc[idx + self.window_size -1 : idx + self.window_size -1 + self.output_dim, -1:].values # next output_dim days
+        if self.window_size == 1:
+            sequences = self.sequenced_data[idx]
+            data = sequences
+        else:
+            sequences = self.sequenced_data[idx: idx + self.window_size]
+            data = pd.concat(sequences, axis=0)
+            # data = torch.nn.utils.rnn.pad_sequence([torch.tensor(seq.values).float() for seq in sequences], batch_first=True)
+        x = data.iloc[:, :].values
+        # x = data
+        if self.window_size == 1:
+            sequences_output = self.sequenced_data[idx: idx + self.output_dim]
+            data_output = sequences_output[0]
+        else:
+            sequences_output = self.sequenced_data[idx : idx + self.window_size + self.output_dim]
+            data_output = pd.concat(sequences_output, axis=0)
+        y = data_output.iloc[-self.output_dim:, -1:].values
         return torch.tensor(x).float().to(device), torch.tensor(y).float().to(device)
+        # return x.to(device), torch.tensor(y).float().to(device)
+    
+    def return_sequences(self, idx):
+        return self.sequenced_data[idx: idx+1]
--- backend/lstm.py
+++ backend/lstm.py
@@ -3,17 +3,34 @@ from torch import nn
 
 # LSTM model class
 class LSTMModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim, dropout=0.2):
         super(LSTMModel, self).__init__()
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
-        
-        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True, dropout=0.2, bidirectional=False)
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.dropout = dropout
+
+        self.conv1 = nn.Conv1d(in_channels=input_dim, out_channels=2, kernel_size=3, stride=1)
+        if num_layers == 1:
+            self.lstm = nn.LSTM(2, hidden_dim, num_layers, batch_first=True, bidirectional=False)
+        else:
+            self.lstm = nn.LSTM(2, hidden_dim, num_layers, batch_first=True, dropout=dropout, bidirectional=False)
         self.fc = nn.Linear(hidden_dim, output_dim)
 
+        # if num_layers == 1:
+        #     self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True, bidirectional=False)
+        # else:
+        #     self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True, dropout=dropout, bidirectional=False)
+        # self.fc = nn.Linear(hidden_dim, output_dim)
+
     def forward(self, x):
+        x = x.permute(0, 2, 1)
+        x = self.conv1(x)
+        x = x.permute(0, 2, 1)
+        
         _, (hidden_n, _) = self.lstm(x)
         
         out = self.fc(hidden_n[-1, :, :])
 
-        return out
+        return out 
